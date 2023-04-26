5fe2e490bf7a1d45e6e994c861accbae385ade74
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2023-04-26 15:48:49 -0600

    Ya funciona la conexion

--- src/main/java/com/mycompany/proyectobd/Conexion.java
+++ src/main/java/com/mycompany/proyectobd/Conexion.java
@@ -1,23 +1,25 @@
 package com.mycompany.proyectobd;
-
+import java.sql.Connection;
+    
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
-
+import com.mysql.cj.jdbc.Driver;
 public class Conexion {
     Connection conectar = null;
-    String usuario = "postgres";
-    String contrasenia = "#Jorge4820";
+    String usuario = "Administrador";
+    String contrasenia = "elbarahonaesputo1@#";
     String bd = "cafeteria";
-    String ip = "localhost";
-    String puerto = "5432";
-    String cadena = "jdbc:postgresql://" + ip + ":" + puerto + "/" + bd;
+    String ip = "cafeteriaproyecto.mysql.database.azure.com";
+    String puerto = "3036";
+    String cadena = "jdbc:mysql://cafeteriaproyecto.mysql.database.azure.com:3306/cafeteria?zeroDateTimeBehavior=CONVERT_TO_NULL";
     
     public Connection establecerConexion(){
         try{
-            Class.forName("org.postgresql.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
             conectar = DriverManager.getConnection(cadena, usuario, contrasenia);
-            //JOptionPane.showMessageDialog(null, "Se conectó correctamente a la base de datos");
+            JOptionPane.showMessageDialog(null, "Se conectó correctamente a la base de datos");
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, "ERROR: " + e.toString());
         }
